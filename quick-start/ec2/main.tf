43a742f36df3a206caf7764b8001b27a1043bc36
vimal-se <vimal.software.engineer@gmail.com>
2024-06-16 20:35:49 +0530

    moved to folders

---
+++ quick-start/ec2/main.tf
@@ -0,0 +1,16 @@
+
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e1d06225679bc1c5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Quick Start EC2"
+  }
+}
+
+
+
---
+++ quick-start/s3/main.tf
@@ -0,0 +1,13 @@
+
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf-bucket-quick-start-my-app-data" {
+  bucket = "tf-bucket-quick-start-my-app-data"
+
+  tags = {
+    Name        = "My Quick Start Bucket"
+  }
+}
